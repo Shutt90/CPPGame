3263bfa078d050eecdd316e5867653feeef95778
Shutt90 <78647249+Shutt90@users.noreply.github.com>
2021-08-29 21:01:22 +0100

    Add project files.

---
+++ Intermediate/Build/Win64/CPPGameEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.29.30133
+#include "C:/Users/liamp/Documents/Unreal Projects/CPPGame/Intermediate/Build/Win64/CPPGameEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
---
+++ Intermediate/Build/Win64/UE4Editor/Inc/CPPGame/CPPGameGameModeBase.generated.h
@@ -0,0 +1,95 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef CPPGAME_CPPGameGameModeBase_generated_h
+#error "CPPGameGameModeBase.generated.h already included, missing '#pragma once' in CPPGameGameModeBase.h"
+#endif
+#define CPPGAME_CPPGameGameModeBase_generated_h
+
+#define CPPGame_Source_CPPGame_CPPGameGameModeBase_h_15_SPARSE_DATA
+#define CPPGame_Source_CPPGame_CPPGameGameModeBase_h_15_RPC_WRAPPERS
+#define CPPGame_Source_CPPGame_CPPGameGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS
+#define CPPGame_Source_CPPGame_CPPGameGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesACPPGameGameModeBase(); \
+	friend struct Z_Construct_UClass_ACPPGameGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(ACPPGameGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/CPPGame"), NO_API) \
+	DECLARE_SERIALIZER(ACPPGameGameModeBase)
+
+
+#define CPPGame_Source_CPPGame_CPPGameGameModeBase_h_15_INCLASS \
+private: \
+	static void StaticRegisterNativesACPPGameGameModeBase(); \
+	friend struct Z_Construct_UClass_ACPPGameGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(ACPPGameGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/CPPGame"), NO_API) \
+	DECLARE_SERIALIZER(ACPPGameGameModeBase)
+
+
+#define CPPGame_Source_CPPGame_CPPGameGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API ACPPGameGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(ACPPGameGameModeBase) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ACPPGameGameModeBase); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ACPPGameGameModeBase); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ACPPGameGameModeBase(ACPPGameGameModeBase&&); \
+	NO_API ACPPGameGameModeBase(const ACPPGameGameModeBase&); \
+public:
+
+
+#define CPPGame_Source_CPPGame_CPPGameGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API ACPPGameGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : Super(ObjectInitializer) { }; \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ACPPGameGameModeBase(ACPPGameGameModeBase&&); \
+	NO_API ACPPGameGameModeBase(const ACPPGameGameModeBase&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ACPPGameGameModeBase); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ACPPGameGameModeBase); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(ACPPGameGameModeBase)
+
+
+#define CPPGame_Source_CPPGame_CPPGameGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET
+#define CPPGame_Source_CPPGame_CPPGameGameModeBase_h_12_PROLOG
+#define CPPGame_Source_CPPGame_CPPGameGameModeBase_h_15_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	CPPGame_Source_CPPGame_CPPGameGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	CPPGame_Source_CPPGame_CPPGameGameModeBase_h_15_SPARSE_DATA \
+	CPPGame_Source_CPPGame_CPPGameGameModeBase_h_15_RPC_WRAPPERS \
+	CPPGame_Source_CPPGame_CPPGameGameModeBase_h_15_INCLASS \
+	CPPGame_Source_CPPGame_CPPGameGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define CPPGame_Source_CPPGame_CPPGameGameModeBase_h_15_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	CPPGame_Source_CPPGame_CPPGameGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	CPPGame_Source_CPPGame_CPPGameGameModeBase_h_15_SPARSE_DATA \
+	CPPGame_Source_CPPGame_CPPGameGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
+	CPPGame_Source_CPPGame_CPPGameGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+	CPPGame_Source_CPPGame_CPPGameGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> CPPGAME_API UClass* StaticClass<class ACPPGameGameModeBase>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID CPPGame_Source_CPPGame_CPPGameGameModeBase_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
